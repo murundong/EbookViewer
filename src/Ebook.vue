dc166223e2598d1f785b0e68599007a53e806fb5
Roden <448544937@qq.com>
2019-03-21 18:00:43 +0800

    menudown

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="ebook">
-    <div class="title-wrapper">
+    <transition name='slide-down'>
+    <div class="title-wrapper"
+      v-show="ifTitleAndMenuShow">
       <div class="left">
         <span class="icon-back icon"></span>
       </div>
@@ -16,22 +18,47 @@
         </div>
       </div>
     </div>
+    </transition>
     <div class="read-wrapper">
       <div id="read">
       </div>
         <div class="mask">
           <div class="left" @click="prevPage"></div>
-          <div class="center"></div>
+          <div class="center" @click="toggleTitleAndMenu"></div>
           <div class="right" @click="nextPage"></div>
         </div>
     </div>
+    <transition name="slide-up">
+      <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="icon-wrapper">
+          <span class="icon-menu icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-progress icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-bright icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-a icon">A</span>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
 import Epub from 'epubjs'
 const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
 export default {
+  data(){
+    return{
+      ifTitleAndMenuShow: false
+    }
+  },
   methods: {
+    toggleTitleAndMenu(){
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+    },
     showEpub () {
       this.book = new Epub(DOWNLOAD_URL)
       this.rendition = this.book.renderTo('read', {
@@ -42,12 +69,12 @@ export default {
     },
     prevPage(){
       if(this.rendition){
-        this.rendition.prev();
+        this.rendition.prev()
       }
     },
     nextPage(){
       if(this.rendition){
-        this.rendition.next();
+        this.rendition.next()
       }
     }
   },
@@ -60,6 +87,33 @@ export default {
   @import "assets/styles/global";
   .ebook {
     position: relative;
+    .title-wrapper{
+      position: absolute;
+      top:0;
+      left:0;
+      width:100%;
+      z-index: 101;
+      height: px2rem(48);
+      background: white;
+      box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15);
+      display: flex;
+      .left{
+        flex: 0 0 px2rem(60);
+        @include center;
+      }
+      .right{
+        flex:1;
+        display:flex;
+        justify-content: flex-end;
+        .iocn-wrapper{
+          flex:0 0 px2rem(40);
+          @include center;
+          .icon-cart{
+            font-size: px2rem(22);
+          }
+        }
+      }
+    }
     .read-wrapper{
       .mask{
         position: absolute;
@@ -79,6 +133,24 @@ export default {
           flex: 0 0 px2rem(100);
         }
 
+      }
+    }
+     .menu-wrapper{
+      position: absolute;
+      bottom:0;
+      left:0;
+      width:100%;
+      z-index: 101;
+      display: flex;
+      height: px2rem(48);
+      background: white;
+      box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+      .icon-wrapper{
+        flex: 1;
+        @include center;
+        .icon-bright,.icon-progress{
+          font-size: px2rem(25);
+        }
       }
     }
   }
